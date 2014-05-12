f9f009b0a96daa9f15fada37c7b32223c8b027e2
joamag <joamag@gmail.com>
2014-05-12 15:23:23 +0100

    small change

--- src/examples/user.py
+++ src/examples/user.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Hive Shopify API
+# Hive GitHub API
 # Copyright (C) 2008-2014 Hive Solutions Lda.
 #
-# This file is part of Hive Shopify API.
+# This file is part of Hive GitHub API.
 #
-# Hive Shopify API is free software: you can redistribute it and/or modify
+# Hive GitHub API is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Hive Shopify API is distributed in the hope that it will be useful,
+# Hive GitHub API is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Hive Shopify API. If not, see <http://www.gnu.org/licenses/>.
+# along with Hive GitHub API. If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = "João Magalhães <joamag@hive.pt>"
 """ The author(s) of the module """
