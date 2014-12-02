d068a3db1fff47329b260320abe3abff7ba0ab46
joamag <joamag@gmail.com>
2014-12-02 17:37:22 +0000

    new base url export

--- src/github/__init__.py
+++ src/github/__init__.py
@@ -39,7 +39,7 @@ from . import orgs
 from . import repo
 from . import user
 
-from .base import Api
+from .base import BASE_URL, Api
 from .orgs import OrgApi
 from .repo import RepoApi
 from .user import UserApi
