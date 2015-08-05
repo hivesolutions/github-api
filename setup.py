0cdf869855db6a7f6685cfe272f0cb39f614fcd6
joamag <joamag@gmail.com>
2015-08-05 15:47:45 +0100

    new copright information

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Hive GitHub API
-# Copyright (C) 2008-2015 Hive Solutions Lda.
+# Copyright (c) 2008-2015 Hive Solutions Lda.
 #
 # This file is part of Hive GitHub API.
 #
