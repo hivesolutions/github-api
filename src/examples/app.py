7cf4b03284a0be70a4f990ce75c1a2d58552add1
joamag <joamag@gmail.com>
2016-03-14 23:54:57 +0000

    multiple kwargs in apps

--- src/examples/app.py
+++ src/examples/app.py
@@ -43,8 +43,12 @@ from . import base
 
 class GithubApp(appier.WebApp):
 
-    def __init__(self):
-        appier.WebApp.__init__(self, name = "github")
+    def __init__(self, *args, **kwargs):
+        appier.WebApp.__init__(
+            self,
+            name = "github",
+            *args, **kwargs
+        )
 
     @appier.route("/", "GET")
     def index(self):
