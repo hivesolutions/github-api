7d0394b0e065274636ad7af1a1cbccb10acbd3d8
joamag <joamag@gmail.com>
2017-09-05 11:25:49 +0100

    new version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "github_api_python",
-    version = "0.1.7",
+    version = "0.1.8",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "GitHub API Client",
