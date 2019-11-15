176c8d5a9098acd2dc048a64a2a4131b0c259fde
joamag <joamag@gmail.com>
2019-11-15 09:25:48 +0000

    New version 0.1.12

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "github_api_python",
-    version = "0.1.11",
+    version = "0.1.12",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "GitHub API Client",
