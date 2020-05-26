9da50f4d199b58c0bfb59f72404802d68ff501ab
joamag <joamag@hive.pt>
2020-05-26 22:15:56 +0100

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "github_api_python",
-    version = "0.1.12",
+    version = "0.2.0",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "GitHub API Client",
