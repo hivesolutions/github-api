e70ca6b38ef970089ce2fae98e7082f567b3b247
João Magalhães <joamag@gmail.com>
2022-12-02 21:53:29 +0000

    feat: renamed repo

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ import os
 import setuptools
 
 setuptools.setup(
-    name = "github_api_python",
+    name = "github-api-python",
     version = "0.2.2",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
@@ -75,5 +75,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7"
-    ]
+    ],
+    long_description = open(os.path.join(os.path.dirname(__file__), "README.md"), "r").read(),
+    long_description_content_type = "text/markdown"
 )
